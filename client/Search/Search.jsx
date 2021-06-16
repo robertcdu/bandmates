6368929fde3606cb02d748d479df318d23d4da03
Yom Woldemichael <yom904@gmail.com>
2021-06-16 15:15:47 -0400

    changes to login and search

--- client/Search/Search.jsx
+++ client/Search/Search.jsx
@@ -5,16 +5,16 @@ import NavBar from '../NavBar/NavBar';
 
 /*
 
-This is the "naive", or quickly done, version of the 
-"search" component. An initial GET request is made 
+This is the "naive", or quickly done, version of the
+"search" component. An initial GET request is made
 for all users, then users are filtered based on some
 criteria. In future iterations, it would be better
 to actually query for users from the DB based on their
-values. 
+values.
 Additionally, when a user resets the form, the values
 do not change, but the state does. This could be changed
 by directly linking the state to the display values, or
-possibly by learning more about Form's onReset. 
+possibly by learning more about Form's onReset.
 
 */
 
@@ -29,15 +29,15 @@ const Search = () => {
 
   useEffect(() => {
     fetch('/api/users')
-      .then(res => res.json())
+      .then((res) => res.json())
       .then(({ users }) => {
         setInitialResults(users);
         setSearchResults(users);
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   }, []);
 
-// sperate lines 34 and 35 to new useffect
+  // sperate lines 34 and 35 to new useffect
 
   const labelToSetState = {
     instruments: setInstrument,
@@ -47,7 +47,7 @@ const Search = () => {
     location: setLocation,
   };
 
-  const handleChange = e => {
+  const handleChange = (e) => {
     e.preventDefault();
     let { name, value } = e.target;
 
@@ -57,141 +57,136 @@ const Search = () => {
     updateState(value);
   };
 
-  const handleSearch = e => {
+  const handleSearch = (e) => {
     e.preventDefault();
 
-    const filteredResults = initialResults.filter(({
-      instruments,
-      skill_level: userSkillLevel,
-      location: userLocation,
-      gender: userGender,
-      genres: userGenres,
-    }) => {
-      //TODO: filtering only sort of work; don't stress test in public! :,(
-      //! Because no users currently have locations, we cannot search by location.
+    const filteredResults = initialResults.filter(
+      ({
+        instruments,
+        skill_level: userSkillLevel,
+        location: userLocation,
+        gender: userGender,
+        genres: userGenres,
+      }) => {
+        //TODO: filtering only sort of work; don't stress test in public! :,(
+        //! Because no users currently have locations, we cannot search by location.
 
-      if (shouldExcludeMen && userGender.toLowerCase() === 'man') {
-        return false;
+        if (shouldExcludeMen && userGender.toLowerCase() === 'man') {
+          return false;
+        }
+
+        const mappedGenres = userGenres.map((g) => g.toLowerCase());
+        const mappedInstruments = instruments.map((i) => i.toLowerCase());
+
+        return (
+          mappedInstruments.includes(instrument) &&
+          mappedGenres.includes(genre) &&
+          userSkillLevel.toLowerCase() === skill
+        );
+        // && userLocation.toLowerCase().contains(location.toLowerCase)
       }
-
-      const mappedGenres = userGenres.map(g => g.toLowerCase());
-      const mappedInstruments = instruments.map(i => i.toLowerCase());
-
-      return mappedInstruments.includes(instrument) &&
-        mappedGenres.includes(genre) &&
-        userSkillLevel.toLowerCase() === skill;
-      // && userLocation.toLowerCase().contains(location.toLowerCase)
-    });
+    );
 
     setSearchResults(filteredResults);
   };
 
-  const handleReset = e => {
+  const handleReset = (e) => {
     e.preventDefault();
     setSearchResults(initialResults);
   };
 
   //TODO: Right now, the select non-males input needs to be clicked
   //twice to register a change. I have no idea why, and it's very annoying.
-  //Do not show this feature during the presentation. 
+  //Do not show this feature during the presentation.
   return (
-    <div className="usersPage">
+    <div className='usersPage'>
       <NavBar />
-      <div id="searchForm">
+      <div id='searchForm'>
         <h2>Search</h2>
         <label>
           Search for musicians with whom to jam...
           <form onSubmit={handleSearch} onReset={handleReset}>
-            <label for="instruments">Choose an instrument:
+            <label for='instruments'>
+              Choose an instrument:
               <select
-                id="instruments"
-                name="instruments"
+                id='instruments'
+                name='instruments'
                 onChange={handleChange}
               >
-                <option value="vocals">Vocals</option>
-                <option value="guitar">Guitar</option>
-                <option value="bass">Bass</option>
-                <option value="drum">Drums</option>
-                <option value="piano">Piano</option>
-                <option value="keyboard / synth">Keyboard / Synth</option>
-                <option value="drum machine">Drum Machine</option>
-                <option value="brass instrument">Brass Instrument</option>
-                <option value="string instrument">String Instrument</option>
-                <option value="percussion">Percussion</option>
+                <option value='vocals'>Vocals</option>
+                <option value='guitar'>Guitar</option>
+                <option value='bass'>Bass</option>
+                <option value='drum'>Drums</option>
+                <option value='piano'>Piano</option>
+                <option value='keyboard / synth'>Keyboard / Synth</option>
+                <option value='drum machine'>Drum Machine</option>
+                <option value='brass instrument'>Brass Instrument</option>
+                <option value='string instrument'>String Instrument</option>
+                <option value='percussion'>Percussion</option>
               </select>
-            </label><br />
-            <label for="genre">Choose a genre:
-              <select
-                id="genre"
-                name="genre"
-                onChange={handleChange}
-              >
-                <option value="rock">Rock</option>
-                <option value="punk">Punk</option>
-                <option value="metal">Metal</option>
-                <option value="hipHop">Hip-Hop</option>
-                <option value="country">Country</option>
-                <option value="soul">Soul</option>
-                <option value="electronic">Electronic</option>
-                <option value="pop">Pop</option>
-                <option value="rnb">RnB / Neo-Soul</option>
-                <option value="reggae">Reggaeton</option>
-                <option value="folk">Folk</option>
+            </label>
+            <br />
+            <label for='genre'>
+              Choose a genre:
+              <select id='genre' name='genre' onChange={handleChange}>
+                <option value='rock'>Rock</option>
+                <option value='punk'>Punk</option>
+                <option value='metal'>Metal</option>
+                <option value='hipHop'>Hip-Hop</option>
+                <option value='country'>Country</option>
+                <option value='soul'>Soul</option>
+                <option value='electronic'>Electronic</option>
+                <option value='pop'>Pop</option>
+                <option value='rnb'>RnB / Neo-Soul</option>
+                <option value='reggae'>Reggaeton</option>
+                <option value='folk'>Folk</option>
               </select>
-            </label><br />
-            <label for="skill">Skill level:
-              <select
-                id="skill"
-                name="skill"
-                onChange={handleChange}
-              >
-                <option value="amateur">Amateur</option>
-                <option value="professional">Professional</option>
+            </label>
+            <br />
+            <label for='skill'>
+              Skill level:
+              <select id='skill' name='skill' onChange={handleChange}>
+                <option value='amateur'>Amateur</option>
+                <option value='professional'>Professional</option>
               </select>
-            </label><br />
-            <label>Location:
+            </label>
+            <br />
+            <label>
+              Location:
               <input
-                type="text"
-                name="location"
-                placeholder="NYC, LA, Outer space"
+                type='text'
+                name='location'
+                placeholder='NYC, LA, Outer space'
                 onChange={handleChange}
               />
-            </label><br />
-            <label>Only show non-men
+            </label>
+            <br />
+            <label>
+              Only show non-men
               <input
-                type="checkbox"
-                id="gender"
-                name="gender"
+                type='checkbox'
+                id='gender'
+                name='gender'
                 onChange={handleChange}
               />
-            </label><br />
-            <input type="submit" value="Search" className="searchButtons" />
-            <input type="reset" value="Reset" className="searchButtons" />
+            </label>
+            <br />
+            <input type='submit' value='Search' className='searchButtons' />
+            <input type='reset' value='Reset' className='searchButtons' />
           </form>
         </label>
       </div>
-      <div className="searchResultsContainer">
+      <div className='searchResultsContainer'>
         {searchResults.map((result, i) => (
-          <SearchResult
-            key={`searchResult${i}`}
-            {...result}
-          />
+          <SearchResult key={`searchResult${i}`} {...result} />
         ))}
       </div>
-      {(initialResults.length && !searchResults.length) && (
-        <div>
-          No musicians found based on your criteria. Bummer!
-        </div>
+      {initialResults.length && !searchResults.length && (
+        <div>No musicians found based on your criteria. Bummer!</div>
       )}
-      {!initialResults.length && (
-        <div>
-          Loading musicians, please wait ...
-        </div>
-      )}
+      {!initialResults.length && <div>Loading musicians, please wait ...</div>}
     </div>
-
-  )
-
+  );
 };
 
 export default Search;
